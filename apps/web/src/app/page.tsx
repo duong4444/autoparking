30187b0b708b12af27357fd2762d46d129d193c5
Duong <duongkc4444@gmail.com>
2025-06-22 22:32:08 +0700

    graphql codegen

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,7 +1,17 @@
+'use client'
+import { useQuery } from '@apollo/client';
+import { CompaniesDocument } from '@autospace/network/src/gql/generated';
 export default function Home() {
+  const { data } = useQuery(CompaniesDocument);
+
   return (
-    <div>
-      <main>Hello front</main>
-    </div>
+    <main>
+      Hello world
+      <div>
+        {data?.companies.map((company) => (
+          <div key={company.id}>{company.displayName}</div>
+        ))}
+      </div>
+    </main>
   );
 }
